076ece3a3fae544c3b37efd552510e4b9e8f9d0c
Toothless <max.rose366@gmail.com>
2020-06-19 16:49:15 +0100

    added 70m round

--- Enums/ERound.cs
+++ Enums/ERound.cs
@@ -2,10 +2,10 @@
 {
     public enum ERound
     {
-        ROUND_18M_30CM = 0, ROUND_18M_60CM 
+        ROUND_18M_30CM = 0, ROUND_18M_60CM, ROUND_70M_122CM
     }
 
     public static class Constatnts{
-        public static readonly string[] ROUND_NAMES = { "18m 30cm", "18m 60cm" };
+        public static readonly string[] ROUND_NAMES = { "18m 30cm", "18m 60cm", "70m 122cm" };
     }
 }
